296f4386e44403dd4bff68ee2691739a5c7fe3f9
n-donnelly <n.donnellyv3.0@gmail.com>
2016-06-21 11:47:35 +0100

    Static URL update
    URL for statics was pointing at the wrong folder - fixed

--- RogerMebert/urls.py
+++ RogerMebert/urls.py
@@ -13,11 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
+from django.conf.urls import include, url, patterns
 from django.contrib import admin
+
+from RogerMebert import settings
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^moviediary/', include('moviediary.urls')),
     
 ]
+
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
